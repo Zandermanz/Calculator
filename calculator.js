4c5da36f60b3055537e59093fdfea87abf56613c
Andrew Phillips <aphillips15@gmail.com>
2022-05-23 19:38:36 -0700

    set display id, clear, and display functions

--- calculator.js
+++ calculator.js
@@ -1,1 +1,17 @@
-//javascript logic
+
+//Sets variable to display in display box
+let display = document.getElementById('display');
+
+let ACButton = document.getElementById('AC')
+ACButton.addEventListener("click", allClear)
+
+//Wipes Display
+function allClear(){
+    //wipes display to 0
+    display.textContent = 0;
+}
+
+//Sets Display
+function setDisplay(value){
+    display.textContent = value;
+}
