3510e19626677cb5797f63bf226593632d1de918
Andrew Phillips <aphillips15@gmail.com>
2022-05-23 22:43:41 -0700

    pressButton function start

--- calculator.js
+++ calculator.js
@@ -2,8 +2,16 @@
 //Sets variable to display in display box
 let display = document.getElementById('display');
 
-let ACButton = document.getElementById('AC')
-ACButton.addEventListener("click", allClear)
+let ACButton = document.getElementById('AC');
+ACButton.addEventListener("click", allClear);
+
+//Idea to add event listeners to buttons, will delete and rework this
+
+// let one = document.getElementById('one');
+// one.addEventListener("click", pressButton);
+
+// let two = document.getElementById('two');
+// two.addEventListener("click", pressButton);
 
 //Wipes Display
 function allClear(){
@@ -14,4 +22,9 @@ function allClear(){
 //Sets Display
 function setDisplay(value){
     display.textContent = value;
+}
+
+function pressButton(button){
+    display.textContent = button;
+    console.log(button);
 }
